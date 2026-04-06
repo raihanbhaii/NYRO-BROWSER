efa358126a4e8bf6568187b52d8e2b225cd6f4a5
Raihan <tipuraihanff@gmail.com>
2026-04-06 23:52:44 +0600

    Create BackgroundScriptRunner.kt

---
+++ app/src/main/java/com/nyro/browser/extensions/BackgroundScriptRunner.kt
@@ -0,0 +1,9 @@
+package com.nyro.browser.extensions
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class BackgroundScriptRunner @Inject constructor() {
+    // Add background script running logic
+}
