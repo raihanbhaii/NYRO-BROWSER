60b5d29092c701015eb334e79cfb7ee247d529fe
Raihan <tipuraihanff@gmail.com>
2026-04-06 23:52:22 +0600

    Create ContentScriptInjector.kt

---
+++ app/src/main/java/com/nyro/browser/extensions/ContentScriptInjector.kt
@@ -0,0 +1,9 @@
+package com.nyro.browser.extensions
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ContentScriptInjector @Inject constructor() {
+    // Add content script injection logic
+}
