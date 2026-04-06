c76d72d4f08a1f001005bd3304309de2162c5831
Raihan <tipuraihanff@gmail.com>
2026-04-06 20:12:48 +0600

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://mozilla.github.io/geckoview/maven2")
+    }
+}
+
+rootProject.name = "NyroBrowser"
+include(":app")
