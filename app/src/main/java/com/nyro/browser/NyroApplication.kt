7220d772d23ca755a114e958980da9b6ea601c90
Raihan <tipuraihanff@gmail.com>
2026-04-06 20:14:08 +0600

    Update NyroApplication.kt

--- app/src/main/java/com/nyro/browser/NyroApplication.kt
+++ app/src/main/java/com/nyro/browser/NyroApplication.kt
@@ -1,89 +1,32 @@
 package com.nyro.browser
 
 import android.app.Application
-import android.content.Context
-import androidx.work.Configuration
-import androidx.work.WorkManager
-import com.nyro.browser.extensions.ExtensionManager
-import com.nyro.browser.services.ConfigService
-import com.nyro.browser.utils.Logger
 import dagger.hilt.android.HiltAndroidApp
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoRuntimeSettings
 
 @HiltAndroidApp
-class NyroApplication : Application(), Configuration.Provider {
+class NyroApplication : Application() {
     
     companion object {
-        private const val TAG = "NyroApplication"
-        lateinit var instance: NyroApplication
+        lateinit var runtime: GeckoRuntime
             private set
-        
-        fun getContext(): Context = instance.applicationContext
-        
-        private var geckoRuntime: GeckoRuntime? = null
-        
-        fun getGeckoRuntime(): GeckoRuntime {
-            return geckoRuntime ?: throw IllegalStateException("GeckoRuntime not initialized")
-        }
     }
-    
+
     override fun onCreate() {
         super.onCreate()
-        instance = this
-        
-        Logger.init(BuildConfig.DEBUG)
-        Logger.d(TAG, "Application initialized")
-        
         initializeGeckoRuntime()
-        initializeServices()
-        initializeWorkManager()
     }
-    
+
     private fun initializeGeckoRuntime() {
-        val builder = GeckoRuntimeSettings.Builder()
+        val settings = GeckoRuntimeSettings.Builder()
             .consoleOutputEnabled(BuildConfig.DEBUG)
-            .debuggingEnabled(BuildConfig.DEBUG)
             .crashReporterEnabled(true)
             .aboutConfigEnabled(BuildConfig.DEBUG)
-            .webExtensionsEnabled(true)
+            .webExtensionsEnabled(BuildConfig.ENABLE_WEB_EXTENSIONS)
             .remoteDebuggingEnabled(BuildConfig.DEBUG)
-        
-        // Configure extension support
-        if (BuildConfig.SUPPORT_CHROME_EXTENSIONS) {
-            builder.webExtensionsEnabled(true)
-            builder.extensionsBaseUri("resource://nyro-extensions/")
-        }
-        
-        val settings = builder.build()
-        geckoRuntime = GeckoRuntime.create(this, settings)
-        
-        Logger.d(TAG, "GeckoRuntime initialized with WebExtensions support")
-    }
-    
-    private fun initializeServices() {
-        ConfigService.initialize(this)
-        ExtensionManager.initialize(this)
-        Logger.d(TAG, "Core services initialized")
-    }
-    
-    private fun initializeWorkManager() {
-        val config = Configuration.Builder()
-            .setMinimumLoggingLevel(android.util.Log.INFO)
             .build()
-        WorkManager.initialize(this, config)
-        Logger.d(TAG, "WorkManager initialized")
-    }
-    
-    override fun getWorkManagerConfiguration(): Configuration {
-        return Configuration.Builder()
-            .setMinimumLoggingLevel(if (BuildConfig.DEBUG) android.util.Log.DEBUG else android.util.Log.INFO)
-            .build()
-    }
-    
-    fun shutdown() {
-        ExtensionManager.getInstance().shutdown()
-        geckoRuntime?.shutdown()
-        Logger.d(TAG, "Application shutdown complete")
+            
+        runtime = GeckoRuntime.create(this, settings)
     }
 }
