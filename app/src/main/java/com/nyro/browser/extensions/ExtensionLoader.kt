10f76ec7ef82773cf29eeaaf50310e18c23a7427
Raihan <tipuraihanff@gmail.com>
2026-04-07 00:55:47 +0600

    Update ExtensionLoader.kt

--- app/src/main/java/com/nyro/browser/extensions/ExtensionLoader.kt
+++ app/src/main/java/com/nyro/browser/extensions/ExtensionLoader.kt
@@ -1,5 +1,6 @@
 package com.nyro.browser.extensions
 
+import com.nyro.browser.utils.Logger
 import android.content.Context
 import com.nyro.browser.extensions.models.Extension
 import com.nyro.browser.utils.Logger
