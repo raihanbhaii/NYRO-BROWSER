d3161e03d3ecb73f70b1ecd43bac6efc79e767b1
Raihan <tipuraihanff@gmail.com>
2026-04-06 19:20:40 +0600

    Delete src directory

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-// src/main.tsx
-import { render } from "solid-js/web";
-import "./index.css";
-import App from "./App";
-
-const root = document.getElementById("root");
-if (!root) throw new Error("Root element not found");
-
-render(() => <App />, root);
