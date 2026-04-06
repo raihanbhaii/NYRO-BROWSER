ea36df32b5c6009782ba486bbdabf26d8c404545
Raihan <tipuraihanff@gmail.com>
2026-04-06 20:12:23 +0600

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,13 +17,7 @@ android {
         
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
-        // Enable extension support features
-        buildConfigField("boolean", "SUPPORT_CHROME_EXTENSIONS", "true")
-        buildConfigField("String", "CHROME_WEB_STORE_API", "\"https://clients2.google.com/service/update2/crx\"")
-        buildConfigField("String", "EXTENSION_STORAGE_PATH", "\"extensions\"")
-        
-        // GeckoView configuration
-        manifestPlaceholders["geckoviewChannel" to "release"]
+        buildConfigField("boolean", "ENABLE_WEB_EXTENSIONS", "true")
     }
 
     buildTypes {
@@ -34,10 +28,8 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("debug")
         }
         debug {
-            isMinifyEnabled = false
             applicationIdSuffix = ".debug"
             versionNameSuffix = "-debug"
         }
@@ -61,73 +53,37 @@ android {
         viewBinding = true
         buildConfig = true
     }
-
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-            excludes += "META-INF/DEPENDENCIES"
-            excludes += "META-INF/LICENSE*"
-            excludes += "META-INF/NOTICE*"
-        }
-    }
-
-    lint {
-        abortOnError = false
-        checkReleaseBuilds = false
-    }
 }
 
 dependencies {
-    // AndroidX Core
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
     implementation("androidx.work:work-runtime-ktx:2.9.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     
-    // Material Design
-    implementation("com.google.android.material:material:1.11.0")
-    
-    // GeckoView for rendering (with WebExtensions support)
+    // GeckoView with WebExtension support
     implementation("org.mozilla.geckoview:geckoview:121.0.20240104093143")
     
-    // Networking
-    implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.9.0")
-    
-    // Serialization
+    // Coroutines & Serialization
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.2")
     
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.7.3")
-    
-    // Dependency Injection
+    // Networking & DI
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.google.dagger:hilt-android:2.50")
     kapt("com.google.dagger:hilt-compiler:2.50")
     
-    // Image Loading
-    implementation("io.coil-kt:coil:2.5.0")
+    // Desugaring
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
     
-    // File Operations
-    implementation("com.github.tamir7:android-file-chooser:1.2.7")
-    
-    // Testing
     testImplementation("junit:junit:4.13.2")
-    testImplementation("org.mockito:mockito-core:5.8.0")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
-    
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("org.mockito:mockito-android:5.8.0")
-    
-    // Core library desugaring for Java 8+ APIs on older Android
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
 }
 
 kapt {
