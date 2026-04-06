771d1c60172cde08898af18737dedae5de8f5d6a
Raihan <tipuraihanff@gmail.com>
2026-04-07 01:28:30 +0600

    Update ManifestParser.kt

--- app/src/main/java/com/nyro/browser/utils/ManifestParser.kt
+++ app/src/main/java/com/nyro/browser/utils/ManifestParser.kt
@@ -1,9 +1,7 @@
 package com.nyro.browser.utils
 
-import com.nyro.browser.utils.Logger
 import com.google.gson.Gson
 import com.nyro.browser.extensions.models.Manifest
-import com.nyro.browser.utils.Logger
 import java.io.File
 import javax.inject.Inject
 import javax.inject.Singleton
