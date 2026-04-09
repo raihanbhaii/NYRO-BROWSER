68d7ef567859171c81923f7b978730b20e10a918
Raihan <tipuraihanff@gmail.com>
2026-04-09 17:46:37 +0600

    Delete app directory

--- app/src/main/java/com/nyro/browser/browser/SecurityInformation.kt
+++
@@ -1,7 +0,0 @@
-package com.nyro.browser.browser
-
-data class SecurityInformation(
-    val isSecure: Boolean = false,
-    val certificateIssuer: String? = null,
-    val certificateSubject: String? = null
-)
